496b2ef266dc684e226677606c764f3f97b9513b
wani <wani@prnd.co.kr>
2024-10-11 19:51:02 +0900

    ColorAsset의 init 를 asset 을 명시하는 방식으로 변경

--- Sources/ColorAsset/ColorAsset+SwiftUI.swift
+++ Sources/ColorAsset/ColorAsset+SwiftUI.swift
@@ -4,24 +4,35 @@ import SwiftUI
 
 
 public extension Color {
+
+    @available(*, deprecated, renamed: "Color(asset:)", message: "This initializer will be removed soon. Please use Color(asset:) instead.")
     init(_ color: ColorAsset) {
+        self.init(asset: color)
+    }
+
+    init(asset color: ColorAsset) {
         switch color.colorType {
-         case let .literal(red, green, blue):
-            self.init(.sRGB, red: red, green: green, blue: blue, opacity: color.alpha)
-            
+        case let .literal(red, green, blue):
+            self = Color(.sRGB, red: red, green: green, blue: blue, opacity: color.alpha)
+
         case let .asset(named, bundle):
-            self.init(named, bundle: bundle)
+            let _color = Color(named, bundle: bundle)
+            if let opacity = color._alpha {
+                self = _color.opacity(opacity)
+            } else {
+                self = _color
+            }
         }
     }
-    
+
     static func asset(_ color: ColorAsset) -> Color {
-        Color(color)
+        Color(asset: color)
     }
 }
 
 
 public extension ColorAsset {
     func toColor() -> Color {
-        Color(self)
+        Color(asset: self)
     }
 }
